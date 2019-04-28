5287799253efce89b300524e7933a553a428ec71
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-28 17:58:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -40,7 +40,7 @@ def adding
 
 
   # return the shipping_manifest below
-  shipping_manifest["diamond necklace"] = 2
+  
   shipping_manifest
 
 end
